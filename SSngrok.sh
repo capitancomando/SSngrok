51343ce594e570d88d382f97e0988da721cd090b
capitan comando <72467460+capitancomando@users.noreply.github.com>
2020-10-08 06:38:08 +0000

    Create SSngrok.sh

---
+++ SSngrok.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+
+#ESTA PROHIBIDA LA COPIA O EDICION DE ESTE SCRIPT.
+
+#USA TU PROPIA ORIGINALIDAD Y MENTE.
+
+/usr/bin/clear
+setterm -foreground green
+echo -e "\nCREADO POR: CAPITAN COMANDO"
+echo "SOCIEDAD ANONIMA NAHFER"
+echo -e "SI LO PUEDES IMAGINAR, LO PUEDES PROGRAMAR.\n"
+echo "PORQUE EL CONOCIMIENTO ES LIBRE. "
+echo "------------------------------------"
+
+opcion="ninguna"
+
+
+
+
+function opcion1
+{  
+   echo -e "\n en la opcion1 \c"
+   read
+   return 0
+}
+
+function opcion2
+{  
+   echo -e "\n en la opcion1 \c"
+   read
+   return 0
+}
+
+function opcion3
+{  
+   echo -e "\n en la opcion1 \c"
+   read
+   return 0
+}
+
+function opcion4
+{  
+   echo -e "\n en la opcion1 \c"
+   read
+   return 0
+}
+
+
+
+
+
+
+while [ $opcion != "5"  ]
+    do 
+	
+	echo -e "\nMENU"
+	echo -e "\t1) SOLUCIONAR LINK NO GENERA NGROK"
+	echo -e "\t2) SOLUCION DE PUERTOS NGROK"
+	echo -e "\t3) INSTALAR NGROK"
+	echo -e "\t4) CONTACTO"
+	echo -e "\t5) SALIR DE LA HERRAMIENTA\n"
+	echo -e "\tOpcion: \c"
+
+	read opcion
+       
+        if [ -z $opcion ] ; then opcion="ninguna" ; fi
+
+        case $opcion in 
+		"1" )
+                      	#!bin/bash
+#by:Capitan Comando
+
+clear
+
+echo -e "\e[32m+ ■■■■■■■■■■■■■■■■■■■■■■■■ +\e[0m"
+echo -e "\e[32m SCRIPT CREADO POR: CAPITAN COMANDO \e[0m"
+sleep 1
+echo -e "\e[32m+ ■■■■■■■■■■■■■■■■■■■■■■■■ +\e[0m"
+echo -e "\e[32m+ CARGANDO SCRIPT...   +\e[0m"
+echo -e "\e[32m+ ■■■■■■■■■■■■■■■■■■■■■■■■ +\e[0m"
+echo ""
+sleep 3.9
+
+echo -e "\e[32m LOADING... \e[0m"
+sleep 4
+clear
+echo "3"
+sleep 1
+clear
+echo "2"
+sleep 2
+clear
+echo "1"
+clear
+
+echo -e "\e[32m CARGANDO... \e[0m"
+echo ""
+sleep 1
+
+mydir=$(cp $PREFIX/bin/ngrok ~/grabcam)
+echo $ cp $PREFIX/bin/ngrok ~/grabcam
+
+echo -e "\e[32m CARGANDO SOLUCION...\e[0m"
+sleep 1
+echo -e "\e[32m+ ■■■■■■■■■■■■■■■■■■■■■■■■+\e[0m"
+echo -e "\e[32m+ BUSCANDO INFORMACION...  +\e[0m"
+echo -e "\e[32m+ ■■■■■■■■■■■■■■■■■■■■■■■■+\e[0m"
+echo -e "\e[32m+ LOADING... +\e[0m"
+echo ""
+sleep 2.9
+
+echo -e "\e[32m+ ESPERE...+\e[0m"
+
+echo -e "\e[32m LOADING... \e[0m"
+sleep 1
+
+echo -e "\e[32m+ ■■■■■■■■■■■■■■■■■■■■■■■■+\e[0m"
+
+echo -e "\e[32m+ CARGANDO SOLUCION DE NGROK...  +\e[0m"
+echo ""
+echo -e "\e[32m+ ■■■■■■■■■■■■■■■■■■■■■■■■ +\e[0m"
+echo ""
+sleep 1
+
+echo -e "\e[32m+ TERMINANDO SOLUCION DE SERVIDORES... +\e[0m"
+echo ""
+sleep 1
+
+cp $PREFIX/bin/ngrok ~/phishing
+
+cp $PREFIX/bin/ngrok ~/ShellPhish
+
+cp $PREFIX/bin/ngrok ~/Zphisher
+
+cp $PREFIX/bin/ngrok ~/AIOPhish
+
+cp $PREFIX/bin/ngrok ~/phisherman
+
+cp $PREFIX/bin/ngrok ~/hiddeneye
+
+cp $PREFIX/bin/ngrok ~/socialsploit
+
+cp $PREFIX/bin/ngrok ~/grabcam
+	     
+  		      ;;
+		"2" ) 
+		      ngrok http 80 
+		      ;;
+   
+		"3" ) 
+              git clone https://github.com/TermuxHacking000/NgrokTH
+              cd NgrokTH
+              chmod 711 ngrok.sh
+              ./ngrok.sh
+		      ;;
+   
+		"4" ) 
+              termux-open http://wa.me/5491133773596
+		      ;;
+   
+   
+                * )  
+		      ;;
+
+	esac
+  
+	 
+	/usr/bin/clear
+done
+  
+
+
+
+ echo -e "\n\nBBBBBB  YY    YY EEEEEEE"
+ echo        "BB  BBB  YY YY   EE     "
+ echo        "BB  BBB   YYY    EE     "
+ echo        "BBBBBB    YYY    EEEEEE "
+ echo        "BB  BBB   YYY    EE     "
+ echo        "BB  BBB   YYY    EE     "
+ echo -e     "BBBBBB    YYY    EEEEEEE\n\n"
+	
